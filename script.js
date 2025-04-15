6460855caf0dc5eb5777cf44a819d6e1a864cb72
Yazz-gt <yazmingaite09@gmail.com>
2025-04-15 08:59:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Página cargada y lista para interactuar.");
+    // Aquí puedes agregar funcionalidades como un carrusel dinámico si es necesario.
+});
